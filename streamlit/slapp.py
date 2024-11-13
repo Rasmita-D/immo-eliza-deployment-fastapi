384a66cd14e4f2d7e788b9151cdc6e090096357f
Rasmita Damaraju <rasmita.damaraju@gmail.com>
2024-11-13 14:53:28 +0100

    -adding all options to the form

--- streamlit/slapp.py
+++ streamlit/slapp.py
@@ -27,21 +27,21 @@ st.write("Belgium Price Prediction Model")
 with st.form("my_form"):
     st.write("Enter the values to get a price prediction:")
     house["property_type"]=st.radio('Property type', ['House', 'Apartment'])
-    house["subproperty_type"] = st.radio('Sub Property type', ['House', 'Apartment'])
-    house["locality"]=st.radio('Locality', ['Gent', 'Bruges'])
+    house["subproperty_type"] = st.radio('Sub Property type', ['APARTMENT', 'APARTMENT_BLOCK', 'BUNGALOW', 'CASTLE', 'CHALET', 'COUNTRY_COTTAGE', 'DUPLEX', 'EXCEPTIONAL_PROPERTY', 'FARMHOUSE', 'FLAT_STUDIO', 'GROUND_FLOOR', 'HOUSE', 'KOT', 'LOFT', 'MANOR_HOUSE', 'MANSION', 'MIXED_USE_BUILDING', 'OTHER_PROPERTY', 'PENTHOUSE', 'SERVICE_FLAT', 'TOWN_HOUSE', 'TRIPLEX', 'VILLA'])
+    house["locality"]=st.radio('Locality', ['Aalst', 'Antwerp', 'Arlon', 'Ath', 'Bastogne', 'Brugge', 'Brussels', 'Charleroi', 'Dendermonde', 'Diksmuide', 'Dinant', 'Eeklo', 'Gent', 'Halle-Vilvoorde', 'Hasselt', 'Huy', 'Ieper', 'Kortrijk', 'Leuven', 'Liège', 'Maaseik', 'Marche-en-Famenne', 'Mechelen', 'Mons', 'Mouscron', 'Namur', 'Neufchâteau', 'Nivelles', 'Oostend', 'Oudenaarde', 'Philippeville', 'Roeselare', 'Sint-Niklaas', 'Soignies', 'Thuin', 'Tielt', 'Tongeren', 'Tournai', 'Turnhout', 'Verviers', 'Veurne', 'Virton', 'Waremme'])
     house["construction_year"]=st.number_input('Construction Year')
     house["total_area_sqm"]=st.number_input('Total area',format="%.2f")
     house["nbr_bedrooms"]=st.number_input('Number of Bedrooms')
-    house["kitchen_type"]=st.radio('Kitchen Level', ['SEMI_EQUIPPED', 'HYPER_EQUIPPED'])
+    house["kitchen_type"]=st.radio('Kitchen Level', ['NOT_INSTALLED','UNINSTALLED','INSTALLED','SEMI_EQUIPPED','HYPER_EQUIPPED'])
     house["fl_furnished"]=0 if st.radio('Furnished?', ['yes', 'no']) =='no' else 1
     house["fl_open_fire"]=0 if st.radio('Fire Place?', ['yes', 'no']) =='no' else 1
     house["terrace_sqm"]=st.number_input('Terrace area',format="%.2f")
     house["garden_sqm"]=st.number_input('Garden area',format="%.2f")
     house["fl_swimming_pool"]=0 if st.radio('Swiiming pool?', ['yes', 'no'])=='no' else 1
     house["fl_floodzone"]=0 if st.radio('Flood Zone?', ['yes', 'no'])=='no' else 1
-    house["state_of_building"]=st.radio('State of the building', ['TO_RENOVATE', 'GOOD'])
+    house["state_of_building"]=st.radio('State of the building', ['TO_RENOVATE','TO_RESTORE','TO_BE_DONE_UP','GOOD','JUST_RENOVATED','AS_NEW'])
     house["primary_energy_consumption_sqm"]=st.number_input('Energy consumption',format="%.2f")
-    house["heating_type"]=st.radio('Heating Type', ['GAS', 'ELECTRICITY'])
+    house["heating_type"]=st.radio('Heating Type', ['GAS', 'FUELOIL', 'ELECTRIC', 'PELLET', 'WOOD', 'SOLAR', 'CARBON'])
     house["fl_double_glazing"]=0 if st.radio('Double Glazing?', ['yes', 'no'])=='no' else 1
     house["cadastral_income"]=st.number_input('Cadastral Income')
 
